628e1390fcfc5ccc21b2917a4be671471d9e1436
184887 <184887@stud.hvl.no>
2024-10-28 21:21:33 +0100

    Constructurs

--- src/main/java/no/hvl/dat100/oppgave1/Innlegg.java
+++ src/main/java/no/hvl/dat100/oppgave1/Innlegg.java
@@ -4,51 +4,68 @@ import no.hvl.dat100.common.TODO;
 
 public abstract class Innlegg {
 	
-	// TODO - deklarering av objektvariable
+	private int id;
+	private String bruker;
+	private String dato;
+	private int likes;
+
 	
 	public Innlegg() {
+
 		
 	}
 	
 	public Innlegg(int id, String bruker, String dato) {
-
-		// TODO 
-		throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
+		
+		this.id = id; 
+		this.bruker = bruker;
+		this.dato = dato;	
+		this.likes = 0;
+		
 	}
 
 	public Innlegg(int id, String bruker, String dato, int likes) {
 
-		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
+		this.id = id; 
+		this.bruker = bruker; 
+		this.dato = dato;
+		this.likes = likes; 
 	}
 	
 	public String getBruker() {
 		
-		throw new UnsupportedOperationException(TODO.method());
+		return bruker;
 
 	}
 
 	public void setBruker(String bruker) {
-		throw new UnsupportedOperationException(TODO.method());
+		if(bruker != null){
+			this.bruker = bruker; 
+		} else {
+			throw new UnsupportedOperationException("Du måp gi bruker et navn"); 
+		}
 	}
 
 	public String getDato() {
-		throw new UnsupportedOperationException(TODO.method());
+		return dato; 
 		
 	}
 
 	public void setDato(String dato) {
-		throw new UnsupportedOperationException(TODO.method());
+		if(bruker != null){
+			this.dato = dato; 
+		} else {
+			throw new UnsupportedOperationException("Datt kan ikke være null"); 
+		}
 	}
 
 	public int getId() {
-		throw new UnsupportedOperationException(TODO.method());
+		return id;
 
 	}
 
 	public int getLikes() {
-		throw new UnsupportedOperationException(TODO.method());
+		return likes;
 
 	}
 	
